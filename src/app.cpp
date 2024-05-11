6eb6740ab8447c8d6703cbb09ea885a27de4e82f
cxxuniverse <anshchadha1996@gmail.com>
2024-05-11 20:14:37 +0200

    updated app.cpp

--- src/app.cpp
+++ src/app.cpp
@@ -34,6 +34,9 @@ void App::game_loop()
     glBindBuffer(GL_ARRAY_BUFFER, buffer);
     glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
 
+    glEnableVertexAttribArray(0);
+    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0);
+
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
