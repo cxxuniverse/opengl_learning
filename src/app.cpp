f598b5c83692aeab6d9e7fbbd936f7be5e38e266
cxxuniverse <anshchadha1996@gmail.com>
2024-05-11 17:53:50 +0200

    updated glBuffer

--- src/app.cpp
+++ src/app.cpp
@@ -23,18 +23,31 @@ void App::create_window(int width, int height, const char *title)
 /* Loop until the user closes the window */
 void App::game_loop()
 {
+
+    float positions[6] = {-0.5f, -0.5f, 0.0f, 0.5f, 0.5f, -0.5f};
+
+    // define vertex buffer
+    GLuint buffer; // unsigned int buffer;
+    glGenBuffers(1, &buffer);
+
+    // selecting / binding the buffer
+    glBindBuffer(GL_ARRAY_BUFFER, buffer);
+    glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float), positions, GL_STATIC_DRAW);
+
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
-        glBegin(GL_TRIANGLES);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
 
-        glVertex2f(-0.5f, -0.5f);
-        glVertex2f(0.0f, 0.5f);
-        glVertex2f(0.5f, -0.5f);
-
-        glEnd();
+        /*  Legacy Code
+            glBegin(GL_TRIANGLES);
+            glVertex2f(-0.5f, -0.5f);
+            glVertex2f(0.0f, 0.5f);
+            glVertex2f(0.5f, -0.5f);
+            glEnd();
+        */
 
         /* Swap front and back buffers */
         glfwSwapBuffers(window);
